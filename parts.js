c077994aeed3409448073874c42b17cb46d56345
Álvaro Cuesta <alvaro-cuesta@GitHub>
2018-08-23 01:16:27 +0200

    remove optional dependencies

--- parts.js
+++ parts.js
@@ -2,10 +2,10 @@
 
 const merge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const IS_DEVELOPMENT = !!process.env.WEBPACK_SERVE;
 
-/**/
 exports.basic = function({ entry, outputPath, alias }) {
   return {
     entry,
@@ -23,19 +23,6 @@ exports.basic = function({ entry, outputPath, alias }) {
   };
 };
 
-/* Dependencies:
- *
- * babel-loader                               ^8.0.0-beta.3
- * @babel/core                                ^7.0.0-beta.47
- * @babel/preset-env                          ^7.0.0-beta.47
- * @babel/plugin-transform-runtime            ^7.0.0-beta.47
- * @babel/plugin-transform-async-to-generator ^7.0.0-beta.53
- * @babel/plugin-proposal-class-properties    ^7.0.0-rc.2
- *
- * Runtime dependencies:
- *
- * @babel/runtime                   ^7.0.0-beta.47
- */
 exports.babelJS = function() {
   process.env.BABEL_ENV = IS_DEVELOPMENT ? 'development' : 'production'
 
@@ -67,13 +54,6 @@ exports.babelJS = function() {
   };
 };
 
-/* Dependencies:
- *
- * All from babelJS() and:
- *
- * @babel/preset-react  ^7.0.0-beta.47
- * react-hot-loader     ^4.2.0
- */
 exports.babelJSX = function() {
   const babel = exports.babelJS();
 
@@ -94,10 +74,6 @@ exports.babelJSX = function() {
   return babel;
 };
 
-/* Dependencies:
- *
- * css-loader   ^0.28.11
- */
 exports.CSS = function() {
   return {
     module: {
@@ -114,12 +90,6 @@ exports.CSS = function() {
   };
 };
 
-/* Dependencies:
-
- * node-sass    ^4.9.0
- * sass-loader  ^7.0.1
- * css-loader   ^0.28.11
- */
 exports.SASS = function() {
   return {
     module: {
@@ -132,12 +102,6 @@ exports.SASS = function() {
     },
   };
 };
-
-/* Dependencies:
- *
- * style-loader ^0.21.0
- */
- const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 exports.styles = function(styleLoaders) {
   for (let { module: { rules } } of styleLoaders) {
